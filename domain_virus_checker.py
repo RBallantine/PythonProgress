119f6fd81c22781aa71c20e88aa2d1a13d5664a2
RonanBal <84083756+RonanBal@users.noreply.github.com>
2022-02-28 13:24:36 +0000

    Update domain_virus_checker.py
    Updated to retrieve IPv4 Address

--- domain_virus_checker.py
+++ domain_virus_checker.py
@@ -2,12 +2,13 @@
     Ronan Ballantine
     25/02/2022
     Domain Virus Checker
-    
-    Program checks a list of domain URLs against the 
-    virus total database using API requests. It then 
-    saves the results in a CSV file.
-    
-    Program not completed yet
+
+    Program checks a list of domain URLs against the
+    virus total database using API requests. It retrieves
+    a total malicious vote count and the IPv4 address
+    of the domain. The results are then saved in a CSV file.
+
+    Program currently being updated
 '''
 
 import requests
@@ -16,29 +17,40 @@ import csv
 
 results = []
 
+# Opens a CSV file containing a list of domain names
 with open('domain_list1.csv', mode='r') as f:
 
     file = csv.reader(f)
     domains = list(file)
 
-    for domain in range(1, len(domains)):
+    # Requests domain information from Virus Total, for each domain on the list
+    for domain in range(0, len(domains)):
 
         virus_total_url = f'https://www.virustotal.com/api/v3/domains/{domains[domain][0]}'
 
         headers = {
             "Accept": "application/json",
-            "x-apikey": "Enter your Virus Total API key here"
+            "x-apikey": "f71dfc8ebb8d6b5c0694be4d1cbdefe0153f34727033ba181e64c30b672ecc64"
         }
 
         response = requests.request("GET", virus_total_url, headers=headers)
         json_response = response.json()
 
-        malicious_count = json_response["data"]['attributes']['last_analysis_stats']['malicious']
+        # Stores the total malicous votes count for the domain
+        malicious_count = json_response['data']['attributes']['last_analysis_stats']['malicious']
 
-        new_result = (domains[domain][0], malicious_count)
+        # Searches the response information for the domains A record IPv4 Address (Only stores one address for now)
+        for lists in range(0, len(json_response['data']['attributes']['last_dns_records'])):
+            if json_response['data']['attributes']['last_dns_records'][lists]['type'] == 'A':
+                ipv4_address = json_response['data']['attributes']['last_dns_records'][lists]['value']
+
+        # Creates a tuple containing the domain name, malicious count and IPv4 address
+        new_result = (domains[domain][0], malicious_count, ipv4_address)
         results.append(new_result)
 
-csv_file = open('domain_list1_results.csv', mode='a', newline='')
+# Creates a new CSV file and writes column headings and the stored data
+csv_file = open('domain_list1_results.csv', mode='w', newline='')
 csv_writer = csv.writer(csv_file, delimiter=',')
+csv_writer.writerows([['Domain', 'Malicious Count', 'IPv4 Address']])
 csv_writer.writerows(results)
 csv_file.close()
